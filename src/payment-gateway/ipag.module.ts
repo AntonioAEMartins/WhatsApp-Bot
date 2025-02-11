c848fc0e82fc38e6c77eac7b4fe26b4b925f7faa
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2025-02-11 18:36:33 -0300

    feature: import transactionmodule into ipag module

--- src/payment-gateway/ipag.module.ts
+++ src/payment-gateway/ipag.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IPagService } from './ipag.service';
 import { IPagController } from './ipag.controller';
+import { TransactionModule } from 'src/transaction/transaction.module';
 
 @Module({
   providers: [IPagService],
   controllers: [IPagController],
   exports: [IPagService],
+  imports: [TransactionModule]
 })
 export class IPagModule {}
