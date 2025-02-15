1cd7cf38f1dfbb02a95ea1e00f511438c87020d6
AntonioAEMartins <antonioaem@icloud.com>
2025-02-15 02:30:34 +0000

    refactor: add CardModule to ipag Module

--- src/payment-gateway/ipag.module.ts
+++ src/payment-gateway/ipag.module.ts
@@ -4,6 +4,7 @@ import { IPagController } from './ipag.controller';
 import { TransactionModule } from 'src/transaction/transaction.module';
 import { BullModule } from '@nestjs/bull';
 import { ConversationModule } from 'src/conversation/conversation.module';
+import { CardModule } from 'src/card/card.module';
 @Module({
   providers: [IPagService],
   controllers: [IPagController],
@@ -16,6 +17,8 @@ import { ConversationModule } from 'src/conversation/conversation.module';
   }),
     BullModule.registerQueue({
       name: 'payment',
-    }),]
+    }),
+    CardModule,
+  ]
 })
 export class IPagModule { }
